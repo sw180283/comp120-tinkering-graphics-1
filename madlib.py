0caad37033fd977fcf177e7b58a8f4353cfc28bc
Michael 'Adrir' Scott <adrir@adrir.com>
2015-11-11 06:37:58 +0000

    Added madlib.py to the repository.

---
+++ madlib.py
@@ -0,0 +1,18 @@
+def madLib():
+  name = "Michael"
+  pet = "Baxter"
+  verb = "ate"
+  snack = "donughts"
+  
+  line1 = "Once upon a time " + name + " was walking"
+  line2 = " with " + pet + ", a trained dragon. "
+  line3 = "Suddenly, " + pet + " stopped and announced, "
+  line4 = "'I have a desperate need for" + snack + "'."
+  line5 = name + " complained. 'Where am I going to get that?' "
+  line6 = "Then " + name + " found a wizard's wand. "
+  line7 = "With a wave of the wand, "
+  line8 = pet + " got " + snack + ". "
+  line9 = "Perhaps surprisingly, " + pet + " " + verb + " the " + snack + "."
+  
+  print line1+line2+line3+line4
+  print line5+line6+line7+line8+line9
